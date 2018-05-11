d90c81ff000df9e85a0c602e107c0c6e057d5f14
Borui Tao <borui.tao@ericsson.com>
2018-05-11 13:01:23 -0400

    Change the licence header to Ericsson
    Change-Id: I4dd95218cc04e6e626c754d0234d83104f604b1d

--- tools/eclipse/project.sh
+++ tools/eclipse/project.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright (C) 2017 The Android Open Source Project
+# Copyright (C) 2017 Ericsson
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,4 +12,5 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 `bazel query @com_googlesource_gerrit_bazlets//tools/eclipse:project --output location | sed s/BUILD:.*//`project.py -n heartbeat -r .
